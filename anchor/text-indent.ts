d672a60e11a95ae49f9634c66461a384cc889a27
justame <yaronn@wix.com>
2022-03-20 17:55:45 +0200

    1 file modified

--- anchor/text-indent.ts
+++ anchor/text-indent.ts
@@ -1,17 +1,16 @@
-import { Extension } from '@tiptap/core'
+import { Extension } from '@tiptap/core';
 
 type TextAlignOptions = {
-  types: string[],
-  }
+  types: string[];
+};
 
 declare module '@tiptap/core' {
   interface Commands<ReturnType> {
     textIndentation: {
+      setTextIndentation: (indentation: number) => ReturnType;
 
-      setTextIndentation (indentation: number) => ReturnType,
-
-       unsetTextIndentation: () => ReturnType,
-    }
+      unsetTextIndentation: () => ReturnType;
+    };
   }
 }
 
@@ -29,41 +28,33 @@ export const TextAlign = Extension.create<TextAlignOptions>({
       {
         types: this.options.types,
         attributes: {
-          textAlign: {
-            default: this.options.defaultAlignment,
-            parseHTML: element => ({
-              textAlign: element.style.textAlign || this.options.defaultAlignment,
-            }),
-            renderHTML: attributes => {
-              if (attributes.textAlign === this.options.defaultAlignment) {
-                return {}
-              }
-
-              return { style: `text-align: ${attributes.textAlign}` }
+          indentation: {
+            default: 0,
+            renderHTML: (attributes) => {
+              return { style: `margin-left: ${attributes.indentation * 10}` };
             },
           },
         },
       },
-    ]
+    ];
   },
 
   addCommands() {
     return {
-      setTextIndentation: (indentation: Indentation) => ({ commands }) => {
-         return this.options.types.every(type => commands.updateAttributes(type, { indentation }))
-      },
-      unsetTextAlign: () => ({ commands }) => {
-        return this.options.types.every(type => commands.resetAttributes(type, 'indentation'))
-      },
-    }
+      setTextIndentation:
+        (indentation: Indentation) =>
+        ({ commands }) => {
+          return this.options.types.every((type) =>
+            commands.updateAttributes(type, { indentation })
+          );
+        },
+      unsetTextAlign:
+        () =>
+        ({ commands }) => {
+          return this.options.types.every((type) =>
+            commands.resetAttributes(type, 'indentation')
+          );
+        },
+    };
   },
-
-  addKeyboardShortcuts() {
-    return {
-      'Mod-Shift-l': () => this.editor.commands.setTextAlign('left'),
-      'Mod-Shift-e': () => this.editor.commands.setTextAlign('center'),
-      'Mod-Shift-r': () => this.editor.commands.setTextAlign('right'),
-      'Mod-Shift-j': () => this.editor.commands.setTextAlign('justify'),
-    }
-  },
-})
+});
